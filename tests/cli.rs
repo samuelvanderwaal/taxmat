6f712864ac6fe0ee4306b93e453357d9399424de
Samuel Vanderwaal <samuel.vanderwaal@gmail.com>
2021-05-17 16:49:45 -0800

    add basic tests

---
+++ tests/cli.rs
@@ -0,0 +1,35 @@
+use assert_cmd::Command;
+
+#[test]
+fn parse_subscan_to_bitcointax() {
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    cmd.args(&[
+        "./tests/subscan.csv",
+        "./tests/output.csv",
+        "-i", 
+        "subscan",
+        "--coin", 
+        "DOT",
+        "-q",
+        "Q1",
+    ])
+    .assert()
+    .success();
+}
+
+#[test]
+fn parse_kraken_to_bitcointax() {
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    cmd.args(&[
+        "./tests/kraken.csv",
+        "./tests/output.csv",
+        "-i", 
+        "kraken",
+        "--coin", 
+        "DOT",
+        "-q",
+        "Q2",
+    ])
+    .assert()
+    .success();
+}
